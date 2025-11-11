49de226f78289abd1e54a585bc017c90d761555a
Amirhossein Gholizadeh <Amirhosseingholizadeh1998@gmail.com>
2025-11-11 20:07:07 +0330

    Added text field to greet user with name

--- app/src/main/java/com/amirsteinbeck/focusmate/MainActivity.kt
+++ app/src/main/java/com/amirsteinbeck/focusmate/MainActivity.kt
@@ -5,6 +5,7 @@ import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import android.widget.EditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -20,13 +21,29 @@ class MainActivity : AppCompatActivity() {
         }
         val button = findViewById<Button>(R.id.textChangerButton)
         val textView = findViewById<TextView>(R.id.textView)
+        val nameField = findViewById<EditText>(R.id.nameInput)
 
         button.setOnClickListener {
-            if (textView.text == getString(R.string.goodbyeMessage)) {
-                textView.text = getString(R.string.helloMessage)
+            val goodbyeToUSerText = getString(R.string.goodbyeToUser, nameField.text)
+            val helloToUSerText = getString(R.string.helloToUser, nameField.text)
+
+            if (nameField.text.isNotEmpty()) {
+                if (textView.text.toString() == getString(R.string.helloMessage)||
+                    textView.text.toString() == getString(R.string.goodbyeMessage) ||
+                    textView.text.toString() == goodbyeToUSerText) {
+                    textView.text = helloToUSerText
+                } else {
+                    textView.text = goodbyeToUSerText
+                }
             } else {
-                textView.text = getString(R.string.goodbyeMessage)
+                if (textView.text == getString(R.string.helloMessage) ||
+                    textView.text == getString(R.string.helloToUser)) {
+                    textView.text = getString(R.string.goodbyeMessage)
+                } else {
+                    textView.text = getString(R.string.helloMessage)
+                }
             }
+
         }
     }
 }
