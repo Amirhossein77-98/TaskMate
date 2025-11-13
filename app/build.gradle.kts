61521cf287c9b3a74d7cc79023db287090a50fa1
Amirhossein Gholizadeh <Amirhosseingholizadeh1998@gmail.com>
2025-11-14 02:54:24 +0330

    Added Material Taste to the UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,10 +44,10 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.material3)
+    implementation("com.google.android.material:material:1.13.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
