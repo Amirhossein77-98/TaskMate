7ce1ce55eee1c87becd4b020fb01e2659990a3b1
Amirhossein Gholizadeh <Amirhosseingholizadeh1998@gmail.com>
2025-11-16 15:25:16 +0330

    Roll back to android default transition animation

--- app/src/main/java/com/amirsteinbeck/focusmate/NavigationHelper.kt
+++ app/src/main/java/com/amirsteinbeck/focusmate/NavigationHelper.kt
@@ -8,17 +8,17 @@ object NavigationHelper {
         val intent = Intent(activity, GreetActivity::class.java)
         intent.putExtra("USERNAME", name)
         activity.startActivity(intent)
-        activity.overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+//        activity.overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
     }
 
     fun goToCredentials(activity: Activity) {
         val intent = Intent(activity, CredentialsActivity::class.java)
         activity.startActivity(intent)
-        activity.overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+//        activity.overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
     }
 
     fun goBack(activity: Activity) {
         activity.finish()
-        activity.overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+//        activity.overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
     }
 }
