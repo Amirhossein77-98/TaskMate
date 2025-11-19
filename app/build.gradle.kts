44e464df108318a79619e8c800a4e29f27aa8859
Amirhossein Gholizadeh <Amirhosseingholizadeh1998@gmail.com>
2025-11-19 18:36:11 +0330

    Changed app version from 1.0 -> 3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.amirsteinbeck.focusmate"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
