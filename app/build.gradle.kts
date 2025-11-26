8addcff91daee2410f8609379f8a43018ea9f294
Amirhossein Gholizadeh <Amirhosseingholizadeh1998@gmail.com>
2025-11-26 22:42:05 +0330

    Changed version from 4.6.8 -> 4.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 4
-        versionName = "4.6.8"
+        versionName = "4.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
