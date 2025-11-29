dc2001c482d5076f8f6a33bcb47084ba7f4ad8bc
Amirhossein Gholizadeh <Amirhosseingholizadeh1998@gmail.com>
2025-11-29 16:30:47 +0330

    Added top row to the settings page

--- app/src/main/java/com/amirsteinbeck/focusmate/SettingsActivity.kt
+++ app/src/main/java/com/amirsteinbeck/focusmate/SettingsActivity.kt
@@ -5,16 +5,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.amirsteinbeck.focusmate.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_settings)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.backButton.setOnClickListener {
+            NavigationHelper.goBack(this)
         }
     }
 }
