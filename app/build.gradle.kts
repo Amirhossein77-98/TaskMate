12d9d41eb0e4b59985c5ef0d0d71cacfda279472
Amirhossein Gholizadeh <Amirhosseingholizadeh1998@gmail.com>
2025-12-10 18:24:16 +0330

    Trying to fix F-droid problems

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 4
-        versionName = "4.10.6"
+        versionName = "4.10.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
